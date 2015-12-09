4b8171cdfa36bde42627bf5ef15b5650fe74ae18
Nathan Caza <mastercactapus@gmail.com>
2015-12-08 19:01:14 -0600

    handle shebang

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import { readFileSync, writeFileSync } from "fs";
 
 var code = readFileSync(process.argv[2]).toString();
 
+var shebang = "";
+if (code[0] === "#") {
+	shebang = /^#.*?$/m.exec(code)[0] + "\n";
+	code = code.slice(shebang.length);
+}
+
+
 var ast = parse(code,{
   sourceType: "module"
 });
 
-writeFileSync(process.argv[2], generate(ast,{
+writeFileSync(process.argv[2], shebang + generate(ast,{
   comments: true,
   compact: false
 }, code).code + "\n");
